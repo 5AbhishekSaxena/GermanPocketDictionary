6cdfbb71ce06a040c87de30bd7d864a0653b73fe
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-09-04 17:24:07 +0530

    Clean up!

--- app/src/main/java/com/abhishek/germanPocketDictionary/activity/agreement/AgreementActivity.kt
+++ app/src/main/java/com/abhishek/germanPocketDictionary/activity/agreement/AgreementActivity.kt
@@ -8,11 +8,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.CompoundButton
-import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.abhishek.germanPocketDictionary.R
@@ -20,9 +18,6 @@ import com.abhishek.germanPocketDictionary.activity.MainActivity
 import java.io.IOException
 
 class AgreementActivity : AppCompatActivity() {
-
-    private var loadingTextView: TextView? = null
-    private var progressBar: ProgressBar? = null
 
     private var alertDialog: AlertDialog? = null
 
@@ -47,7 +42,7 @@ class AgreementActivity : AppCompatActivity() {
     }
 
     private fun setupAgreementAlertDialog() {
-        var agreement: String? = null
+        val agreement: String
         try {
             agreement = viewModel.getAgreement()
         } catch (e: IOException) {
@@ -91,37 +86,9 @@ class AgreementActivity : AppCompatActivity() {
     private fun agreementLoadingFailed() {
         Toast.makeText(this, "Agreement failed to load, contact the developer", Toast.LENGTH_LONG)
             .show()
-        hideProgressBarAndShowTextView(R.string.agreement_failed_to_load)
     }
 
     private fun updateAgreementStatus(status: Boolean) {
         alertDialog?.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = status
-    }
-
-    private fun hideProgressBarAndShowTextView(@StringRes stringRes: Int) {
-        hideProgressBar()
-        showLoadingTextView(stringRes)
-    }
-
-    private fun hideProgressBar() {
-        if (progressBar == null) progressBar = findViewById(R.id.loading_indicator)
-
-        progressBar?.visibility = View.GONE
-    }
-
-    private fun showProgressBar() {
-        if (progressBar == null) progressBar = findViewById(R.id.loading_indicator)
-        progressBar?.visibility = View.VISIBLE
-    }
-
-    private fun showLoadingTextView(@StringRes stringRes: Int) {
-        showLoadingTextView()
-        loadingTextView?.setText(stringRes)
-    }
-
-    private fun showLoadingTextView() {
-        if (loadingTextView == null) loadingTextView =
-            findViewById(R.id.loading_text_view)
-        loadingTextView?.visibility = View.VISIBLE
     }
 }
