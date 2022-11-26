88058758cf2b966d2cc3a1ab85623579f5032138
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-11-27 01:55:05 +0530

    Add app theme and surface around HomeScreen.

--- app/src/main/java/com/abhishek/germanPocketDictionary/activity/HomeActivity.kt
+++ app/src/main/java/com/abhishek/germanPocketDictionary/activity/HomeActivity.kt
@@ -3,7 +3,9 @@ package com.abhishek.germanPocketDictionary.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
 import com.abhishek.germanPocketDictionary.activity.home.ui.HomeScreen
+import com.abhishek.germanPocketDictionary.core.ui.theme.GPDTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,7 +15,11 @@ class HomeActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            HomeScreen()
+            GPDTheme {
+                Surface {
+                    HomeScreen()
+                }
+            }
         }
     }
 }
