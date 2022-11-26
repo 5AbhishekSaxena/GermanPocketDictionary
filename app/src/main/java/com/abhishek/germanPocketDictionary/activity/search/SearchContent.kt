ea716a0f62a4d23eb4c9ec6bca27f98c38b73959
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-11-27 03:43:55 +0530

    Autofocus search bar when opened and minor cosmetic changes.

--- app/src/main/java/com/abhishek/germanPocketDictionary/activity/search/SearchContent.kt
+++ app/src/main/java/com/abhishek/germanPocketDictionary/activity/search/SearchContent.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -27,15 +28,23 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.abhishek.germanPocketDictionary.R
 import com.abhishek.germanPocketDictionary.activity.home.ui.WordList
 import com.abhishek.germanPocketDictionary.core.ui.components.ErrorText
 import com.abhishek.germanPocketDictionary.core.ui.components.words.UIMinWord
 import com.abhishek.germanPocketDictionary.core.ui.theme.GPDTheme
+import kotlinx.coroutines.delay
 
 @Composable
 fun SearchContent(
@@ -97,6 +106,14 @@ private fun LoadedContent(
         containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.3f),
     )
 
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+
+    LaunchedEffect(key1 = Unit) {
+        delay(300L)
+        focusRequester.requestFocus()
+    }
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -106,7 +123,8 @@ private fun LoadedContent(
         Row(
             modifier = Modifier
                 .fillMaxWidth(0.85f)
-                .padding(16.dp)
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
         ) {
             Icon(
                 imageVector = Icons.Default.Search,
@@ -122,12 +140,19 @@ private fun LoadedContent(
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Search
                 ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        focusManager.clearFocus()
+                    },
+                ),
                 decorationBox = { innerTextField ->
                     if (viewState.query.isBlank()) Text(text = "Search Words..")
 
                     innerTextField()
                 },
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .focusRequester(focusRequester),
             )
         }
     }
