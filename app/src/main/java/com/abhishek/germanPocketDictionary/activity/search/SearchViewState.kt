ccee6f09b930d30d43acad1b1477cfc20c02edd1
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-11-27 02:47:53 +0530

    Add SearchContent composable.

---
+++ app/src/main/java/com/abhishek/germanPocketDictionary/activity/search/SearchViewState.kt
@@ -0,0 +1,26 @@
+package com.abhishek.germanPocketDictionary.activity.search
+
+import com.abhishek.germanPocketDictionary.core.ui.components.words.UIMinWord
+
+sealed class SearchViewState {
+    object Initial : SearchViewState()
+
+    object Loading : SearchViewState()
+
+    sealed class Loaded(
+        open val query: String,
+        open val words: List<UIMinWord.Simple>
+    ) : SearchViewState() {
+        data class Idle(
+            override val query: String,
+            override val words: List<UIMinWord.Simple>
+        ) : Loaded(query, words)
+
+        data class Searching(
+            override val query: String,
+            override val words: List<UIMinWord.Simple>
+        ) : Loaded(query, words)
+    }
+
+    data class Error(val error: Exception) : SearchViewState()
+}
