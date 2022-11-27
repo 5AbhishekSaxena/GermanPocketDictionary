f6a5b5adc448b7fa8b06c01a33776f94a6a6a92e
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-11-27 21:21:58 +0530

    Add SplashScreen composable.

---
+++ app/src/main/java/com/abhishek/germanPocketDictionary/activity/splash/SplashViewModel.kt
@@ -0,0 +1,31 @@
+package com.abhishek.germanPocketDictionary.activity.splash
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abhishek.germanPocketDictionary.activity.agreement.domain.repository.AgreementRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val agreementRepository: AgreementRepository
+) : ViewModel() {
+
+    private val _viewState = MutableStateFlow<SplashViewState>(SplashViewState.Initial)
+    val viewState = _viewState.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            val accepted = agreementRepository.getAgreementAcceptanceStatus()
+            delay(500L)
+            _viewState.value = if (accepted)
+                SplashViewState.Accepted
+            else
+                SplashViewState.Pending
+        }
+    }
+}
