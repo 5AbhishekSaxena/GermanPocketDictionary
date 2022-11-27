752bc5dc83be111bd1301061cb1f986558fe05be
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-11-27 21:15:39 +0530

    Setup navigation.

---
+++ app/src/main/java/com/abhishek/germanPocketDictionary/activity/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.abhishek.germanPocketDictionary.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.material3.Surface
+import com.abhishek.germanPocketDictionary.core.ui.theme.GPDTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.rememberAnimatedNavHostEngine
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            val navHostEngine = rememberAnimatedNavHostEngine(
+                rootDefaultAnimations = RootNavGraphDefaultAnimations(
+                    enterTransition = { slideInHorizontally(initialOffsetX = { it }) },
+                    exitTransition = { slideOutHorizontally(targetOffsetX = { -it }) },
+                    popEnterTransition = { slideInHorizontally(initialOffsetX = { -it }) },
+                    popExitTransition = { slideOutHorizontally(targetOffsetX = { it }) }
+                )
+            )
+
+            GPDTheme {
+                Surface {
+                    DestinationsNavHost(
+                        navGraph = NavGraphs.root,
+                        engine = navHostEngine
+                    )
+                }
+            }
+        }
+    }
+}
